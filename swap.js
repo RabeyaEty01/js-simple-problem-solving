88ab62805e8cfdfe2980681340330fd2b9586be1
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-06 12:43:02 +0600

    swap and destructuring

---
+++ swap.js
@@ -0,0 +1,18 @@
+/*
+//swaping with temp
+let first = 5;
+let second = 7;
+console.log(first, second);
+//first approach
+let temp = first;
+first = second;
+second = temp;
+console.log(first,second);
+*/
+
+//destructuring
+let first = 5;
+let second = 7;
+//console.log(first, second);
+[first, second] = [second, first];
+console.log(first, second);
