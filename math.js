953c5b7ffd27bcedb0f1b305aea422593d7e0214
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-06 12:24:52 +0600

    math added

---
+++ math.js
@@ -0,0 +1,22 @@
+// const myNumber = -5;
+// const output =Math.abs(myNumber);
+
+const myNumber=6.0398;
+
+//const output =Math.ceil(myNumber);
+//const output =Math.floor(myNumber);
+//const output =Math.round(myNumber);
+
+//const output =Math.random(); //0 to 1 er moddhe random value dibe
+/*
+const output =Math.random()*10; //0-10 er moddhe value dekhabe 
+//console.log(output);
+const rounded=Math.round(output);
+console.log(rounded);
+*/
+
+for (let i =0; i<=30; i++){
+    const output =Math.random()*6; 
+const rounded=Math.round(output);
+console.log(rounded);
+}
